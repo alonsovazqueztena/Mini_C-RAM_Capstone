5ae59740b5e95244c57dd9ec1638e5046ef4fe6c
Alonso Vazquez Tena <alonsoxa16@gmail.com>
2025-01-06 19:52:54 -0700

    Fixed test.py
    Fixed file path for test image in test class.

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 model = YOLO("yolo_epoch_100.pt")
-results = model.predict("00000001_jpg.rf.2d8fb1dff4c5ebf31f0c0e406b2b6c21.jpg", conf=0.5, imgsz=640)
+results = model.predict("drone_test.jpg", conf=0.5, imgsz=640)
 print(results[0].boxes)  # Check if bounding boxes exist
