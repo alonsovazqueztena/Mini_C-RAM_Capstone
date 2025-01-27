e9668de1c4307f072968cb9f5716222ca099578b
Alonso Vazquez Tena <alonsoxa16@gmail.com>
2025-01-27 13:22:04 -0700

    Organized files into folders

--- test.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO
-model = YOLO("yolo_epoch_100.pt")
-results = model.predict("drone_test.jpg", conf=0.5, imgsz=640)
-print(results[0].boxes)  # Check if bounding boxes exist
